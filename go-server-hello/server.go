d040b19646a90316245c8f58fa42c848f0f0528a
rinriN <2210368@ecc.ac.jp>
2022-11-17 00:07:50 +0900

    :pencil2: 在主程序中實例化一個靜態文件服務器, 並讓他在網頁跟目錄上被響應

--- go-server-hello/server.go
+++ go-server-hello/server.go
@@ -1,5 +1,14 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println(">>Hello Welcome to Go Server<<")
+
+	// 實例化一個文件服務器, 且讓文件服務器使用本幣文件夾./static
+	fileServer := http.FileServer(http.Dir("./static"))
 
 }
