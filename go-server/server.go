f5c1777d8dd13897b5f146a93a474a0c3039ccd8
rinriN <2210368@ecc.ac.jp>
2022-11-17 17:43:50 +0900

    :pencil2: 修改小錯誤

--- go-server/server.go
+++ go-server/server.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello Welcome to Go Server")
+	fmt.Println(">>Hello Welcome to Go Server<<")
 
 	// 實例化一個文件服務器, 且讓文件服務器使用本幣文件夾./static
 	fileServer := http.FileServer(http.Dir("./static"))
