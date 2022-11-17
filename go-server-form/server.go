8ae689a156543c7075067549210c16f3e0e01f7d
rinriN <2210368@ecc.ac.jp>
2022-11-17 20:52:21 +0900

    :pencil2: 新增loginHandler 響應使用者的表單提交

--- go-server-form/server.go
+++ go-server-form/server.go
@@ -22,6 +22,24 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	// 只允許POST請求
+	if r.Method != "POST" {
+		http.Error(w, "方法不支持", http.StatusNotFound)
+	}
+	// 解析表單失敗報error
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	name := r.FormValue("name")
+	pwd := r.FormValue("pwd")
+
+	fmt.Fprintf(w, "用戶名：%s\n", name)
+	fmt.Fprintf(w, "密碼：%s\n", pwd)
+}
+
 func main() {
 	fmt.Println(">>Welcome to Go Server<<")
 
@@ -32,6 +50,9 @@ func main() {
 
 	// ②在使用者請求/hello目錄時響應調用 helloHandler HandleFunc
 	http.HandleFunc("/hello", helloHandler)
+
+	// ③在使用者請求/login目錄時響應調用 loginHandler HandleFunc
+	http.HandleFunc("/login", loginHandler)
 
 	fmt.Printf("服務器運行在端口 :8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
