0f0013c836840e96f32381519500b1d26e81435a
rinriN <2210368@ecc.ac.jp>
2022-11-18 23:59:25 +0900

    :pencil2: 定義設置共同的Http response Header 訊息的函數

--- go-restapi-crud/server.go
+++ go-restapi-crud/server.go
@@ -1,10 +1,16 @@
 package main
 
+import "net/http"
+
 // 定義標準返回結果類型結果類型的結構體
 type ResponseResult struct {
 	Result string `json:"result"`
 }
 
+// 設置共同的Http response Header 訊息
+func setSameHeader(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+}
 func main() {
 
 }
